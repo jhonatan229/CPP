b7dcf6b8fe05e66f719fd16a318b2c1f73b63031
jhonatan229 <jhonatan229@outlook.com>
2022-04-13 21:40:51 -0300

    fix ex02

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -5,25 +5,6 @@
 #include <iostream>
 #include <stack>
 
-// template< typename T, typename container = std::deque<T> >
-// class MutantStack : public std::stack< T, container >
-// {
-// 	public:
-// 		MutantStack( void ) : std::stack< T, container>( ) { return ; };
-// 		MutantStack( MutantStack const & rhs ) : std::stack< T, container>( rhs ) { return ; };
-// 		~MutantStack( void ) { return ; };
-// 		MutantStack & operator=( MutantStack const & rhs )	{
-// 			std::stack< T, container>::operator=( rhs );
-// 			return ( *this );
-// 		}
-
-// 		typedef typename container::iterator iterator;
-
-// 		typename container::iterator begin( void ) { return ( this->c.begin() ); };
-// 		typename container::iterator end( void ) { return ( this->c.end() ); };
-		
-// };
-
 template<typename T, typename container = std::deque<T> >
 class MutantStack : public std::stack<T, container>
 {
@@ -34,7 +15,7 @@ public:
 
 	MutantStack &operator=(const MutantStack &assign){ 
 		std::stack<T, container>::operator=(assign);
-		return *this;
+		return (*this);
 	}
 
 	typedef typename container::iterator iterator;
